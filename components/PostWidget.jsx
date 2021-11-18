0c54521a7eabd1bc280b037f038b376657948253
Imran Rahman <mrimranoor@gmail.com>
2021-11-18 22:21:22 +0000

    Display posts & view either recent posts or related posts in the widget component

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import moment from "moment";
+import Link from "next/link";
 
-const PostWidget = () => {
-  return <div>PostWidget</div>;
+import { getRecentPosts, getSimilartPosts } from "../services";
+
+const PostWidget = ({ categories, slug }) => {
+  const [relatedPosts, setRelatedPosts] = useState([]);
+
+  useEffect(() => {
+    if (slug) {
+      getSimilarPosts(categories, slug).then((result) =>
+        setRelatedPosts(result)
+      );
+    } else {
+      getRecentPosts().then((result) => setRelatedPosts(result));
+    }
+  }, [slug]);
+
+  console.log(relatedPosts);
+
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+      <h3 className="text-xl font-semibold mb-8 border-b pb-4">
+        {slug ? "Related Posts" : "Recent Posts"}
+      </h3>
+      {relatedPosts.map((post) => (
+        <div className="flex items-center w-full mb-4" key={post.title}>
+          <div className="w-16 flex-none">
+            <img
+              src={post.featuredImage.url}
+              alt={post.title}
+              height="60px"
+              width="60px"
+              className="align-middle rounded-full"
+            />
+          </div>
+          <div className="flex-grow ml-4">
+            <p className="text-gray-500 font-xs">
+              {moment(post.createdAt).format("DD MMM YYYY")}
+            </p>
+            <Link
+              className="text-md"
+              key={post.title}
+              href={`/post/${post.slug}`}
+            >
+              {post.title}
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default PostWidget;
--- services/index.js
+++ services/index.js
@@ -41,3 +41,42 @@ export const getPosts = async () => {
 
     return result.postsConnection.edges;
 }
+
+export const getRecentPosts = async () => {
+  const query = gql`
+  query GetRecentPosts(){
+    posts(orderBy: createdAt_ASC, last: 3)
+    {
+      title
+      featuredImage {
+        url
+      }
+      createdAt
+      slug
+    }
+  }
+  `
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+}
+
+export const getSimilartPosts = async () => {
+  const query = gql`
+  query GetPostDetails($slug: String!, $categories: [String!]){
+    post(where: {slug_not: $slug, AND: {categories_some: {slug_in: $categories}}})}
+    last: 3)
+    {
+      title
+      featuredImage {
+        url
+      }
+      createdAt
+      slug
+    }
+  }
+  `
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+}
